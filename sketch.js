59f639402fe84c7ebeb4d3fa1183219934b6ef14
klederson-soares <36171803+klederson-soares@users.noreply.github.com>
2019-02-22 20:17:50 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,46 +1,62 @@
-var raio_numero = 500;
-var raio_interno = raio_numero +50;
-var raio_externo = raio_interno + 50;
-var cx, cy;
-var px, py;
-var destaque_theta;
-
+	var raioInteno = 200;
+  var raioCentro = raioInteno + 70
+  var raioExterno = raioCentro + 40
+  var cx, cy;
+  var destaque_theta;
+	var horas=1
 function setup() {
-    createCanvas(600, 600);
-    textAlign(CENTER, CENTER);
-    // centro do desenho e do circulo
-    cx = width / 2;
-    cy = height / 2;
-    // ângulo theta para arco de destaque
-    // que é o mesmo theta para o ponto p
-    destaque_theta = PI * 1.75;
-    px = cos(destaque_theta) * raio_numero;
-    py = sin(destaque_theta) * raio_numero;
-    // corrige p(px,py) para p(px+cx,py+cy);
-    px += cx;
-    py += cy;
-}
+  createCanvas(500, 500);
+  textAlign(CENTER, CENTER);
+  strokeWeight(3);
+  cx = width / 2;
+  cy = height / 2;
+  }
 
 function draw() {
-    background(255);
-    fill(255);
-    for (var angulo = 0; angulo <= 360; angulo += 30) {
-        // transforma o angulo de
-        // coord polar para coord cartesiana
-        var x = cos(radians(angulo)) * raio_numero;
-        var y = sin(radians(angulo)) * raio_numero;
-    
-        // desenha uma elipse na coord (x,y)
-        // corrige (x,y) para (x+cx,y+cy)
-        ellipse(x + cx, y + cy, 50);
-      }
+  background(220);
+  fill(255)
+  ellipse(cx, cy, raioExterno);
+  
+  fill(255)
+  ellipse(cx, cy, raioCentro);
+  
+  fill(255)
+	//ellipse(cx, cy, raioInteno);
+  
+  fill(0);
+  aux=4
+  cont=30
+  for (var angulo = 0; angulo <= 360; angulo += 30) {
 
-    fill(0,0,255, 20);
-    ellipse(px, py, 150);
-    fill(0);
-    ellipse(cx, cy, raio_externo);
-    fill(255,0,0);
-    ellipse(cx, cy, raio_interno);
-    fill(0,255,0);
-    ellipse(cx, cy, raio_numero);
+    var x = cos(radians(angulo)) * raioInteno/1.7;
+    var y = sin(radians(angulo)) * raioInteno/1.7;
+
+    if(angulo==cont){
+
+    	if (aux==12){
+      	textSize(30)
+        text(aux, x + cx, y + cy)
+        aux=0
+        aux+=1
+        cont=cont+30
+      }
+    	else{
+        textSize(30)
+        text(aux, x + cx, y + cy);
+        aux+=1
+        cont=cont+30
+      }
+  	}
+	}
+  
+  
+  var x = cos(radians(angulo)) * raioInteno/1.7;
+  var y = sin(radians(angulo)) * raioInteno/1.7;
+    
+  
+  ellipse(cx, cy, 20);
+  line(y+cy, cy, cx, cy);//horas       
+	line(cx, cx-x, cx, cy);//minutos
+//line(cimaX, cimaY, baixoX, baixoY);
+  
 }
